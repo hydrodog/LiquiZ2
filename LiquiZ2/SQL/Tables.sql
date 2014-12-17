dc1f300f998674ab904c20fbe1e81ac8cbaac9cc
Jinnie <yijkan@bergen.org>
2014-12-17 09:16:35 -0500

    database no longer specifies InnoDB engine

--- LiquiZ2/SQL/Tables.sql
+++ LiquiZ2/SQL/Tables.sql
@@ -29,8 +29,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`Policies` (
   `ShuffleAns` BIT(1) NOT NULL DEFAULT 0,
   `AccessCode` VARCHAR(255) NULL,
   PRIMARY KEY (`PolID`),
-  UNIQUE INDEX `PolID_UNIQUE` (`PolID` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `PolID_UNIQUE` (`PolID` ASC));
 
 
 -- -----------------------------------------------------
@@ -40,9 +39,8 @@ DROP TABLE IF EXISTS `LiquiZ`.`DisplayElements` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`DisplayElements` (
   `DispElID` INT NOT NULL AUTO_INCREMENT COMMENT 'Not sure I\'ll need this but needed for diagram I think',
-  `Type` CHAR(4) NOT NULL,
-  PRIMARY KEY (`DispElID`))
-ENGINE = InnoDB;
+  `Type` CHAR(4) NOT NULL, -- text, image, video, audio, etc
+  PRIMARY KEY (`DispElID`));
 
 
 -- -----------------------------------------------------
@@ -54,11 +52,10 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`Quizzes` (
   `QuizID` INT NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(255) NULL,
   `Desc` INT NULL,
-  `Policy` INT NOT NULL,
+  `Policy` INT NOT NULL, -- Policies.PolID
   `Privacy` CHAR(1) NOT NULL,
   PRIMARY KEY (`QuizID`),
-  UNIQUE INDEX `QID_UNIQUE` (`QuizID` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `QID_UNIQUE` (`QuizID` ASC));
 
 
 -- -----------------------------------------------------
@@ -72,8 +69,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`Questions` (
   `Level` INT NULL,
   `QType` CHAR(4) NOT NULL,
   PRIMARY KEY (`QuesID`),
-  UNIQUE INDEX `QuesID_UNIQUE` (`QuesID` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `QuesID_UNIQUE` (`QuesID` ASC));
 
 
 -- -----------------------------------------------------
@@ -86,8 +82,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`Courses` (
   `Name` VARCHAR(255) NOT NULL,
   `Privacy` CHAR(1) NOT NULL,
   PRIMARY KEY (`CourseID`),
-  UNIQUE INDEX `CID_UNIQUE` (`CourseID` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `CID_UNIQUE` (`CourseID` ASC));
 
 
 -- -----------------------------------------------------
@@ -96,10 +91,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `LiquiZ`.`CoursesQuizzes` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`CoursesQuizzes` (
-  `Course` INT NULL,
-  `Quiz` INT NULL,
-  `Sequence` INT NULL)
-ENGINE = InnoDB;
+  `Course` INT NULL, -- Courses.CourseID
+  `Quiz` INT NULL, -- Quizzes.QuizID
+  `Sequence` INT NULL);
 
 
 -- -----------------------------------------------------
@@ -110,8 +104,7 @@ DROP TABLE IF EXISTS `LiquiZ`.`QuesCon` ;
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`QuesCon` (
   `QuesConID` INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`QuesConID`)
-  )
-ENGINE = InnoDB;
+  );
 
 
 -- -----------------------------------------------------
@@ -120,10 +113,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `LiquiZ`.`QuizzesQuesCons` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`QuizzesQuesCons` (
-  `Quiz` INT NOT NULL,
-  `QuesCon` INT NOT NULL,
-  `Sequence` INT NOT NULL)
-ENGINE = InnoDB;
+  `Quiz` INT NOT NULL, -- Quizzes.QuizID
+  `QuesCon` INT NOT NULL, -- QuesCon.QuesConID
+  `Sequence` INT NOT NULL);
 
 
 -- -----------------------------------------------------
@@ -132,12 +124,11 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `LiquiZ`.`QuesConElements` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`QuesConElements` (
-  `QuesCon` INT NOT NULL,
+  `QuesCon` INT NOT NULL, -- QuesCon.QuesConID
   `Sequence` INT NOT NULL,
-  `Element` INT NULL,
-  `Ques` INT NULL,
-  `Type` CHAR(4) NOT NULL)
-ENGINE = InnoDB;
+  `Element` INT NULL, -- DisplayElements.DispElID
+  `Ques` INT NULL, -- Questions.QuesID
+  `Type` CHAR(4) NOT NULL); -- disp or ques
 
 
 -- -----------------------------------------------------
@@ -147,12 +138,11 @@ DROP TABLE IF EXISTS `LiquiZ`.`Answers` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`Answers` (
   `AnsID` INT NOT NULL AUTO_INCREMENT,
-  `Response` INT NULL,
-  `Element` INT NULL COMMENT 'The DispEl that represents the answer',
+  `Response` INT NULL, -- DisplayElements.DispElID
+  `Element` INT NULL COMMENT 'The DispEl that represents the answer', -- DisplayElements.DispElID
   `LowBound` INT NULL COMMENT 'to accept a range of numbers (lower bound)',
   `HighBound` INT NULL COMMENT 'to accept a range of numbers (upper bound)',
-  PRIMARY KEY (`AnsID`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`AnsID`));
 
 
 -- -----------------------------------------------------
@@ -161,10 +151,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `LiquiZ`.`DispElSeq` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`DispElSeq` (
-  `DispEl` INT NOT NULL,
-  `Element` VARCHAR(255) NOT NULL,
-  `Sequence` INT NOT NULL)
-ENGINE = InnoDB;
+  `DispEl` INT NOT NULL, -- DisplayElements.DispElID
+  `Element` VARCHAR(255) NOT NULL, -- String represents text or file name
+  `Sequence` INT NOT NULL); -- For longer strings
 
 
 -- -----------------------------------------------------
@@ -175,8 +164,7 @@ DROP TABLE IF EXISTS `LiquiZ`.`StdSet` ;
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`StdSet` (
   `StdSetID`  INT NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`StdSetId`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`StdSetId`));
 
 
 -- -----------------------------------------------------
@@ -185,10 +173,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `LiquiZ`.`StdChoices` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`StdChoices` (
-  `StdSetID` INT NOT NULL,
-  `Element` INT NOT NULL,
-  `Sequence` INT NOT NULL)
-ENGINE = InnoDB;
+  `StdSetID` INT NOT NULL, -- StdSet.StdSetID
+  `Element` INT NOT NULL, -- DisplayElements.DispElID
+  `Sequence` INT NOT NULL);
 
 
 -- -----------------------------------------------------
@@ -202,8 +189,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`QuesAnsSeq` (
   `StdSet` INT NULL,
   `StdChoice` INT NULL, -- Sequence, so we can set correct
   `Sequence` INT NULL,
-  `Correct` BIT(1) NULL) -- 0/1
-ENGINE = InnoDB;
+  `Correct` BIT(1) NULL); -- 0/1
 
 
 -- -----------------------------------------------------
@@ -218,8 +204,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`Users` (
   `Username` VARCHAR(255) NOT NULL,
   `Password` VARCHAR(255) NOT NULL,
   `Email` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`UserID`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`UserID`));
 
 
 -- -----------------------------------------------------
@@ -231,8 +216,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`UserPermissions` (
   `User` INT NOT NULL,
   `Entity` INT NULL COMMENT 'Course or Quiz... other Users?',
   `EType` CHAR(4) NULL COMMENT 'Course or Quiz... other Users?',
-  `Permissions` INT NULL COMMENT 'each bit represents a permission')
-ENGINE = InnoDB;
+  `Permissions` INT NULL COMMENT 'each bit represents a permission');
 
 
 -- -----------------------------------------------------
@@ -243,8 +227,7 @@ DROP TABLE IF EXISTS `LiquiZ`.`Responses` ;
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`Responses` (
   `ResponseID` INT NOT NULL AUTO_INCREMENT,
   `Element` INT NOT NULL,
-  PRIMARY KEY (`ResponseID`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`ResponseID`));
 
 
 -- -----------------------------------------------------
@@ -256,8 +239,7 @@ CREATE TABLE IF NOT EXISTS `LiquiZ`.`StudentResponses` (
   `Student` INT NOT NULL,
   `Ques` INT NOT NULL,
   `Response` INT NOT NULL,
-  `Correct` BIT(1) NULL)
-ENGINE = InnoDB;
+  `Correct` BIT(1) NULL);
 
 
 -- -----------------------------------------------------
@@ -268,8 +250,7 @@ DROP TABLE IF EXISTS `LiquiZ`.`StudentGradesOnQuizzes` ;
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`StudentGradesOnQuizzes` (
   `Student` INT NOT NULL,
   `Quiz` INT NOT NULL,
-  `Score` INT NOT NULL)
-ENGINE = InnoDB;
+  `Score` INT NOT NULL);
 
 
 -- -----------------------------------------------------
@@ -279,8 +260,7 @@ DROP TABLE IF EXISTS `LiquiZ`.`StudentGrades` ;
 
 CREATE TABLE IF NOT EXISTS `LiquiZ`.`StudentGrades` (
   `Student` INT NOT NULL,
-  `Grade` DOUBLE NOT NULL)
-ENGINE = InnoDB;
+  `Grade` DOUBLE NOT NULL);
 
 DROP TABLE IF EXISTS `LiquiZ`.`ViewAnsToQues` ;
 DROP TABLE IF EXISTS `LiquiZ`.`ViewQuiz` ;
