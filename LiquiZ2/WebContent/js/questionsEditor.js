8eb1818c690bb359f92f6f0fd4750828b1a5f032
hydrodog <dov.kruger@gmail.com>
2014-12-23 16:18:22 -0500

    added regex type, streamlined question interface

--- LiquiZ2/WebContent/js/questionsEditor.js
+++ LiquiZ2/WebContent/js/questionsEditor.js
@@ -1,11 +1,17 @@
 //must have a blank option one for question of type dropdown
 
-standard('qTypes',['','Drop Down', 'Multidrop-down', 'Number Fillin', 'Essay', 'Code']);
+standard('qTypes',['','Drop Down', 'Multidrop-down', 'Number Fillin', 'Essay', 'Code', 'Regex']);
 
-standard('qTypes2',['','dropdowns', 'multidropdowns', 'numfillins', 'essays', 'codes']);
+standard('qTypes2',['','dropdowns', 'multidropdowns', 'numfillins', 'essays', 'codes', 'regex']);
 standard('correct',['Correct','Incorrect']);
 standard('incorrect',['Incorrect','Correct']);
 
+regex = {
+  physics : [
+      'mass', 'length', 'time',
+      'massqty', 'lengthqty', 'timeqty'
+  ]
+};
 
 quiz({qList:[
   
@@ -22,15 +28,15 @@ quiz({qList:[
     question({type:'essay',qHTML:'Placeholder Text:',rows:3,cols:30,pHold:'Your text here...',qID:"placeholderText"}),
     question({type:'html',HTML:'Choices:'}),
     question({type:'multiquestion',qHTML:'',qArry:[
-      question({type:'essay',rows:3,cols:30,pHold:'Your text here...',qID:"choices:1"}),
+      question({type:'essay',rows:3,cols:30,pHold:'Your answer here...',qID:"choices:1"}),
       question({type:'dropdown',qHTML:'',ansrL:choices['correct'],qID:"correct:1"})
     ]}),
     question({type:'multiquestion',qHTML:'',qArry:[
-      question({type:'essay',rows:3,cols:30,pHold:'Your text here...',qID:"choices:2"}),
+      question({type:'essay',rows:3,cols:30,pHold:'Your answer here...',qID:"choices:2"}),
       question({type:'dropdown',qHTML:'',ansrL:choices['incorrect'],qID:"correct:2"})
     ]}),
     question({type:'multiquestion',qHTML:'',qArry:[
-      question({type:'essay',rows:3,cols:30,pHold:'Your text here...',qID:"choices:3"}),
+      question({type:'essay',rows:3,cols:30,pHold:'Your answer here...',qID:"choices:3"}),
       question({type:'dropdown',qHTML:'',ansrL:choices['incorrect'],qID:"correct:3"})
     ]}),
   question({type:'html',HTML:'<br>'}),
@@ -41,7 +47,23 @@ quiz({qList:[
   questionSet({class:'codes',qList:[
     question({type:'essay',qHTML:'Base Code:',rows:3,cols:30,pHold:'Your base code here...',qID:"baseCode"})
   ]}),
-  questionSet({class:'numfillins essays',qList:[
+  questionSet({class:'numfillins',qList:[
+    question({type:'essay',qHTML:'Correct Answer:',rows:1,cols:30,pHold:'Answer ...',qID: "correctanswer"}),
+    question({type:'essay',qHTML:'Min Value:',rows:1,cols:30,pHold:'Min ...',qID: "minvalue"}),
+    question({type:'essay',qHTML:'Max Value:',rows:1,cols:30,pHold:'Max ...',qID: "maxvalue"}),
+    question({type:'essay',qHTML:'Placeholder Text:',rows:1,cols:30,pHold:'Your placeholder text here...',qID:"placeholderText"})
+
+  ]}),
+  questionSet({class:'regex',qList:[
+    dropdown({qHTML:'',ansrL:regex['physics'],qID:"regexquestion"}),
+//TODO: change name essay to "textarea"
+//TODO: add "textfield"
+    essay({qHTML:'Pattern:',rows:1,cols:30,pHold:'', qID:"pattern"}),
+    essay({qHTML:'Name:',rows:1,cols:30,pHold:'', qID:"patternname"})
+//TODO: add function button to create a button
+//    button(qHTML:'Save:',rows:3,cols:30,pHold:'',qID:"patternname"}),
+  ]}),
+  questionSet({class:'essays',qList:[
     question({type:'essay',qHTML:'Placeholder Text:',rows:3,cols:30,pHold:'Your placeholder text here...',qID:"placeholderText"})
   ]})
   
