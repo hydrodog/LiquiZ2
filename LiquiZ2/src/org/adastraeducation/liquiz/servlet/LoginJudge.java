e83c59814fd1d9513fbeb2036c09a1ce30775994
Jinnie <yijkan@bergen.org>
2015-01-07 12:37:07 -0500

    fixed LoginJudge import

--- LiquiZ2/src/org/adastraeducation/liquiz/servlet/LoginJudge.java
+++ LiquiZ2/src/org/adastraeducation/liquiz/servlet/LoginJudge.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.adastraeducation.liquiz.database.UserLogin;
+import org.adastraeducation.liquiz.database.Check;
 
 /*
  * Author: Yingzhu
