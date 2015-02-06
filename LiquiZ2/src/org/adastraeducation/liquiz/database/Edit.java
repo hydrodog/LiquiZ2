8a875970e08c4e558ba6abe6fb1de5c25fe73fc7
wy193777 <421036289@qq.com>
2015-02-05 20:47:16 -0500

    made some change in function to update course in database

--- LiquiZ2/src/org/adastraeducation/liquiz/database/Edit.java
+++ LiquiZ2/src/org/adastraeducation/liquiz/database/Edit.java
@@ -4,17 +4,22 @@ import java.sql.*;
 import org.adastraeducation.liquiz.*;
 
 /**
- * Edits rows when entities are edited
+ * update course infomation
+ * @param name course name
+ * @param privacy A 4-character string representing a privacy setting
  * @author yijinkang
  *
  */
 public class Edit {
-	public void updateCourse() { 
+	public void updateCourse(String name, String privacy) { 
 		Connection conn = null;
 		try {
 			conn = DatabaseMgr.getConnection();
-			PreparedStatement p = conn.prepareStatement("UPDATE Users SET ?=? WHERE ?=?");
+			PreparedStatement p = conn.prepareStatement("UPDATE Users SET Name = ? WHERE Privacy = ?");
 			//TODO! What parameters? 
+			p.setString(1, name);
+			p.setString(2, privacy);
+			p.execute();
 		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
