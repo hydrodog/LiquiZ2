5e3ad0caeb8cb966c83781126d29fdfa7f84932f
Jinnie <yijkan@bergen.org>
2015-03-19 10:50:01 -0400

    StdChoice and RegexPatterns put into one NamedObjects class
    Other global entities will be stored here

---
+++ LiquiZ2/src/org/adastraeducation/liquiz/NamedObjects.java
@@ -0,0 +1,58 @@
+package org.adastraeducation.liquiz;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+/**
+ * Contains static HashMaps of all named, shared items
+ * 
+ * Standard choices are named sets of answers
+ * Poll: strongly agree, agree, neutral, disagree, strongly disagree
+ * YesNo: Yes, No
+ * 
+ * Patterns are named answer patterns
+ * This allows non-programmers to look up patterns rather than writing their own
+ * Mass: (\d{0,2}(\.\d{0,2}))(kg|kilogram)
+ * @author yijinkang
+ *
+ */
+public class NamedObjects {
+	private static HashMap<String, ArrayList<Answer>> lookUpStdChoice;
+//	public static QuestionPattern MASS = new QuestionPattern("([0-9|\.]+)(kg|kilogram)");
+//	public static QuestionPattern LENGTH = new QuestionPattern("([0-9|\.]+)(m|meter)");
+//	public static QuestionPattern TIME = new QuestionPattern("([0-9|\.]+)(s|second)");
+//	public static QuestionPattern VELOCITY = new QuestionPattern("([0-9|\.]+)(m\\/s|meter\\/second)");
+	private static HashMap<String, Pattern> lookUpPattern;
+	
+	static {
+		lookUpStdChoice = new HashMap<String, ArrayList<Answer>>();
+		lookUpPattern = new HashMap<String, Pattern>();
+		
+		lookUpPattern.put("Mass", Pattern.compile("(\\d{0,2}(\\.\\d{0,2}))(kg|kilogram)"));
+		lookUpPattern.put("Length", Pattern.compile("(\\d{0,2}(\\.\\d{0,2}))(m|meter)"));
+	}
+
+	public static void addStdChoice(String name, ArrayList<Answer> answers) {
+		for (Answer ans : answers) {
+			ans.setCorrect(false);
+		}
+		lookUpStdChoice.put(name, answers);
+	}
+	public static ArrayList<Answer> getStdChoice(String name) {
+		return lookUpStdChoice.get(name);
+	}
+	public static ArrayList<Answer> getCloneOfStdChoice(String name) {
+		ArrayList<Answer> copy = new ArrayList<Answer>();
+		ArrayList<Answer> orig = lookUpStdChoice.get(name);
+		
+		for (Answer ans : orig) {
+			copy.add(ans.clone());
+		}
+		
+		return copy;
+	}
+	
+	//TODO: write methods to write out hashmaps to database
+}
---
+++ LiquiZ2/src/org/adastraeducation/liquiz/NumberRange.java
@@ -0,0 +1,46 @@
+package org.adastraeducation.liquiz;
+
+import java.util.ArrayList;
+
+/*
+ * author:Yingzhu
+ * This is for Number question
+ * Useless temporarily. Do not use them, Use Fillin instead.
+ */
+
+public class NumberRange extends FillIn {
+	private double min;
+	private double max;
+	
+	public NumberRange(int id, int points, int level, double min, double max){
+		super(id,points,level,(ArrayList<Answer>) null);
+		this.min = min;
+		this.max = max;
+	}
+	
+	/*
+	 * Returns true if typed number is within correct range
+	 */
+	public boolean isCorrect(String s) {
+		double d = Double.parseDouble(s);
+		return min<=d && d<=max;
+	}
+
+	public double getMin() {
+		return min;
+	}
+
+	public void setMin(double min) {
+		this.min = min;
+	}
+
+	public double getMax() {
+		return max;
+	}
+
+	public void setMax(double max) {
+		this.max = max;
+	}
+	
+	
+}
