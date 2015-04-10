b13fef4bd3969b20e25a9381c7dec704e6caff75
Poornima Kuna <poornimakuna@gmail.com>
2015-04-10 14:04:07 -0400

    reading the input of std choice and regex pattern from jsp page

---
+++ LiquiZ2/src/org/adastraeducation/liquiz/FormInput.java
@@ -0,0 +1,48 @@
+package org.adastraeducation.liquiz;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class FormInput {
+	
+	public static boolean ReadStdChoices(HttpServletRequest req){
+		String name=req.getParameter("name");
+		System.out.println("Name:" +name);
+		if(name==null)
+			return false;
+		ArrayList<Answer> StdChoices = new ArrayList<Answer>();
+		for(int i=1;;i++){
+			String choice = req.getParameter("a"+i);
+			if(choice==null||choice.equals("")){
+				break;
+			}
+			StdChoices.add(new Answer(new Text(choice)));
+		}
+		for(int i=0;i<StdChoices.size();i++){
+			System.out.println(StdChoices.get(i));
+		}
+	NamedObjects.addStdChoice(name,StdChoices);
+		return true;
+	}
+	
+	public static boolean ReadRegexPatterns(HttpServletRequest req){
+		String category=req.getParameter("Category");
+		if(category==null)
+				return false;
+		String name=req.getParameter("Rname");
+		String Rpattern = req.getParameter("Rpattern");
+		Pattern pattern = Pattern.compile(Rpattern);
+		System.out.println(category);
+		System.out.println(name);
+		System.out.println(Rpattern);
+		System.out.println(pattern);
+		HashMap<String,Pattern> allPatterns = new  HashMap<String,Pattern>();
+		allPatterns.put(name, pattern);
+		NamedObjects.addRegexPattern(category,allPatterns);
+		
+		return true;
+	}
+}
