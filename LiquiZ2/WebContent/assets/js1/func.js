69c0e8a18cd05f72ba40c3dc47f35304eca2a58f
Poornima Kuna <poornimakuna@gmail.com>
2015-05-13 14:40:19 -0400

    Update func.js

--- LiquiZ2/WebContent/assets/js1/func.js
+++ LiquiZ2/WebContent/assets/js1/func.js
@@ -158,5 +158,21 @@ function initPage() {
 function submitComments() {
 	alert(commentList);
 } 
+/*For menu dropdown showing and hiding in testListQuizzes.html*/
+function expand(s)
+{
+  var td = s;
+  var d = td.getElementsByTagName("div").item(0);
 
+  td.className = "menuHover";
+  d.className = "menuHover";
+}
+function collapse(s)
+{
+  var td = s;
+  var d = td.getElementsByTagName("div").item(0);
+
+  td.className = "menuNormal";
+  d.className = "menuNormal";
+}
 
