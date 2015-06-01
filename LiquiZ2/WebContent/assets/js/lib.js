1c9ef7a80d7905185660cbd3eeabbe7014f1987f
Asher Davidson <usherdavidson@gmail.com>
2015-06-01 14:15:20 -0400

    Fixed small indentation error.

--- LiquiZ2/WebContent/assets/js/lib.js
+++ LiquiZ2/WebContent/assets/js/lib.js
@@ -36,11 +36,11 @@ function mktable(className, arr) {
     var t = document.createElement("table");
     t.className = className;
     for (var i = 0; i < arr.length; i++) {
-	var tr = t.insertRow(i);
-	for (var j = 0; j < arr[i].length; ++j) {
-	    var c = tr.insertCell(j);
-	    c.appendChild(arr[i][j]);
-	}
+    var tr = t.insertRow(i);
+    for (var j = 0; j < arr[i].length; ++j) {
+        var c = tr.insertCell(j);
+        c.appendChild(arr[i][j]);
+    }
     }
     return t;
 }
@@ -77,8 +77,8 @@ function ins(txt)  { return {type: 'ins', text: txt}  } // instructions <span cl
 function lin(txt)  { return {type: 'lin', text: txt}  } // text within <p>
 function box(txt)  { return {type: 'box', text: txt}  } // text within div
 function gri(mat)  { return {type: 'mat', arr: mat,
-			     className: 'grid', readOnly: 1,
-			     rows: mat.length, cols: mat[0].length}   } // create a table based on matrix
+                 className: 'grid', readOnly: 1,
+                 rows: mat.length, cols: mat[0].length}   } // create a table based on matrix
 function img(file) { return {type: 'img', file: file} }
 function aud(file) { return {type: 'aud', file: file} }
 function vid(file) { return {type: 'vid', file: file} }
@@ -106,7 +106,7 @@ function match(id, questions, answers) {
 function dump(obj) {
     var s = "";
     for (var k in obj) {
-	s += k + "-->" + obj[k] + '\n';
+    s += k + "-->" + obj[k] + '\n';
     }
     alert(s);
 }
@@ -132,7 +132,7 @@ function mkbutton(val) {
 function make(tag, inner, className) {
     var t = document.createElement(tag);
     if (typeof(className) != 'undefined')
-	t.className = className;
+    t.className = className;
     t.innerHTML = inner;
     return t;
 }
@@ -164,9 +164,9 @@ function Quiz(quizinfo, qlist) {
     quizinfo.display(this.div);
     this.createSubmit();
     for (var i = 0; i < qlist.length; i++) {
-	var qc = mkdiv(this.div, "qc");
-	for (var j = 0; j < qlist[i].length; j++)
-	    this.add(qc, qlist[i][j]);
+        var qc = mkdiv(this.div, "qc");
+        for (var j = 0; j < qlist[i].length; j++)
+            this.add(qc, qlist[i][j]);
     }
     this.createSubmit();
 }
@@ -195,10 +195,10 @@ Quiz.prototype.createSubmit = function() {
 
 Quiz.prototype.qhe = function(qhead) {
     return mktable("qheader",
-		   [ [ mk("h2", qhead.title, ''),
-		       mk("span", "points:" + qhead.points, "qpoints"),
-		       mk("span", "level:" +qhead.level, "level")
-		     ]]);
+           [ [ mk("h2", qhead.title, ''),
+               mk("span", "points:" + qhead.points, "qpoints"),
+               mk("span", "level:" +qhead.level, "level")
+             ]]);
 }
 
 Quiz.prototype.img = function(img) {
@@ -265,8 +265,8 @@ Quiz.prototype.add = function(parent, spec) {
 
 Quiz.prototype.mcr = function(mcr) {
     for (var i = 0; i < mcr.list.length; i++) {
-	this.div.appendChild(mkinput(mcr.id, 'radio', 'multichoiceradio'));
-	this.add(this.div, mcr.list[i]);
+    this.div.appendChild(mkinput(mcr.id, 'radio', 'multichoiceradio'));
+    this.add(this.div, mcr.list[i]);
     }
     return this.div;
 }
@@ -276,10 +276,10 @@ Quiz.prototype.select = function(id, list) {
     s.id = id;
     s.className = "multichoicedropdown";
     for (var i = 0; i < list.length; i++) {
-	var opt = document.createElement("option");
-	opt.value = i;
-	opt.appendChild(this[list[i].type](list[i]));
-	s.appendChild(opt);
+    var opt = document.createElement("option");
+    opt.value = i;
+    opt.appendChild(this[list[i].type](list[i]));
+    s.appendChild(opt);
     }
     return s;
 }
@@ -291,11 +291,11 @@ Quiz.prototype.mcd = function(mcd) {
 Quiz.prototype.match = function(match) { 
     var t = document.createElement("table");
     for (var i = 0; i < match.questions.length; ++i) {
-	var r = t.insertRow(i);
-	var q = r.insertCell(0);
-	this.add(q, match.questions[i]);
-	q = r.insertCell(1);
-	q.appendChild(this.select(match.id + "_" + i, match.answers));
+    var r = t.insertRow(i);
+    var q = r.insertCell(0);
+    this.add(q, match.questions[i]);
+    q = r.insertCell(1);
+    q.appendChild(this.select(match.id + "_" + i, match.answers));
     }
     return t;
 }
@@ -306,16 +306,16 @@ Quiz.prototype.mat = function(mat) {
     var m = mat.arr;
     var hasVal = typeof(m) != 'undefined';
     for (var i = 0; i < mat.rows; i++) {
-	var r = t.insertRow(i);
-	r.className = "mat.rowClassName";
-	for (var j = 0; j < mat.cols; j++) {
-	    var td = r.insertCell(j);
-	    var v = hasVal ? m[i][j] : '';
-	    var inp = this.numid(id + "_" + i + "," + j, v);
-	    if (mat.readOnly)
-		inp.readOnly = mat.readOnly;
-	    td.appendChild(inp);
-	}	
+    var r = t.insertRow(i);
+    r.className = "mat.rowClassName";
+    for (var j = 0; j < mat.cols; j++) {
+        var td = r.insertCell(j);
+        var v = hasVal ? m[i][j] : '';
+        var inp = this.numid(id + "_" + i + "," + j, v);
+        if (mat.readOnly)
+        inp.readOnly = mat.readOnly;
+        td.appendChild(inp);
+    }   
     }
     return t;
 }
@@ -346,10 +346,10 @@ Quiz.prototype.cloze = function (cloze) {
     pre.className = "code";
     var f = fil(cloze.id);
     while ((ind = t.search(clozeTarget)) >= 0) {
-	if (ind > 0)
-	    pre.appendChild(this.txt(t.substring(0, ind-last)));
-	pre.appendChild(this.fil(cloze.id + "_" + count++));
-	t = t.substring(ind + 4);
+    if (ind > 0)
+        pre.appendChild(this.txt(t.substring(0, ind-last)));
+    pre.appendChild(this.fil(cloze.id + "_" + count++));
+    t = t.substring(ind + 4);
     }
     pre.appendChild(this.txt(t));
     return pre;
@@ -378,102 +378,102 @@ Quiz.prototype.ess = function(ess) {
 function build() {
     var quizinfo  = new QuizInfo("Quiz Demo #1", 100, 0, 1, "assets/");
     var qlist = [
-	[
-	    qhead("Mergesort", 1, 1),
-	    lin("Show the first pass of Mergesort below"),
-	    gri([[9, 8, 7, 6, 5, 4, 3, 1]]),
- 	    mat(1, 1, 8)
-	],
-	[
-	    qhead("Matrix Multiplication", 1, 1),
-	    lin("Show the result of matrix A * B"),
-	    gri([[1, 0, 2],[1, 1, -2],[2, 1, 0]]), txt("*"),
-	    gri([[1, 1, -1],[-2, 1, 0],[1, 1, 3]]), txt("="),
- 	    mat(2, 3, 3)
-	],
-	[
-	    qhead('Java'),
-	    lin('Complete the code below so it prints "Hello"'),
-	    cod(3, "public A {\n  void   (String[] args) {\n  System.\n  }\n}\n", 10, 80)
-	],
-	[
-	    qhead('Java'),
-	    lin('Complete the following function so it computes factorial recursively.'),
-	    cod(4, "public static void fact(int n) {\n\n\n\n}", 10, 80)
-	],
-	[
-	    qhead('Java'),
-	    txt("Fill in the blanks to make the code correct"),
-	    cloze(5, 'public [[]] A {\n  [[]] static [[]] main([[]] [] args) {\n  System.[[]].[[]]("hello");\n  }\n}')
-	],
-	[
-	    qhead("Object Oriented Terminology"),
-	    lin("Match the object-oriented terminology to the meaning"),
-	    match(6, [txt("class"), txt("object"), txt("method"), txt("message"),
-		   txt("polymorphism"), txt("encapsulation")],
-		  [txt("A concrete instance of a class"),
-		   txt("A request made to an object"),
-		   txt("Hiding the internal details of a class or object"),
-		   txt("Sending the same message to different objects and getting different results"),
-		   txt("A specification of an object"),
-		   txt("A function that is applied to an object")
-		  ])
-	],
-	[
-	    qhead("File Upload"),
-	    lin("Submit your homework for the 3n+1 problem as a single .java file"),
-	    fup(7, "java"),	    
-	],
-//	    match(17, ["class", "object", "method", "message", "polymorphism", "encapsulation"],
-//		  ["A concrete instance of a class",
-//		   "A request made to an object",
-//		   "Hiding the internal details of a class or object",
-//		   "Sending the same message to different objects and getting different results",
-//		   "A specification of an object"
-//		  ])
-	[
-	    qhead("Graph Theory"),
-	    lin("Find the Shortest Path from vertex 1 to 5.  Leave any cost box blank if the vertex is unreachable."),
-	    img("Bellmanford_3.png"),
-	    mat(8, 3, 5)
-	],
-	[
-	    qhead("Arithmetic"),
-	    txt("What is 2 + 2?"),
-	    fil(10)
-	],
-	[
-	    qhead('Dinosaur'),
-	    lin("Which one is the dinosaur?"),
-//	    mcr(11, [img("cat2.jpg"), img("fish2.png"), img("trex.jpg")])
-	],
-	[
-	    qhead('Dinosaur'),
-	    lin("Which one is the dinosaur?"),
-	    mcd(12, [img("cat2.jpg"), img("fish2.png"), img("trex.jpg")])
-	],
-	[
-	    qhead('Multimedia'),
-	    txt('listen to the following audio file and pick the name of the main character.'),
-	    aud("clip1.mp3"),
-	    mcd(13, [txt('Yijin'), txt('Asher'), txt('Ying'), txt('Xuefan'), txt('Bob')])
-	],
-	[
-	    qhead('Tacoma Narrows'),
-	    txt('Watch the following video, then explain what caused the bridge to fail.'),
-	    vid("Tacoma Narrows Bridge Collapse.mp4"),
-	    ess(14, 10, 80, 200)
-	],
-	[
-	    qhead('Arithmetic'),
-	    txt("What is the square root of 2 (four digits is fine)?"),
-	    num(15)
-	],
-	[
-	    qhead("Geography"),
-	    lin("Click on Texas"),
-	    cli(16,"usmap.gif")
-	]
+    [
+        qhead("Mergesort", 1, 1),
+        lin("Show the first pass of Mergesort below"),
+        gri([[9, 8, 7, 6, 5, 4, 3, 1]]),
+        mat(1, 1, 8)
+    ],
+    [
+        qhead("Matrix Multiplication", 1, 1),
+        lin("Show the result of matrix A * B"),
+        gri([[1, 0, 2],[1, 1, -2],[2, 1, 0]]), txt("*"),
+        gri([[1, 1, -1],[-2, 1, 0],[1, 1, 3]]), txt("="),
+        mat(2, 3, 3)
+    ],
+    [
+        qhead('Java'),
+        lin('Complete the code below so it prints "Hello"'),
+        cod(3, "public A {\n  void   (String[] args) {\n  System.\n  }\n}\n", 10, 80)
+    ],
+    [
+        qhead('Java'),
+        lin('Complete the following function so it computes factorial recursively.'),
+        cod(4, "public static void fact(int n) {\n\n\n\n}", 10, 80)
+    ],
+    [
+        qhead('Java'),
+        txt("Fill in the blanks to make the code correct"),
+        cloze(5, 'public [[]] A {\n  [[]] static [[]] main([[]] [] args) {\n  System.[[]].[[]]("hello");\n  }\n}')
+    ],
+    [
+        qhead("Object Oriented Terminology"),
+        lin("Match the object-oriented terminology to the meaning"),
+        match(6, [txt("class"), txt("object"), txt("method"), txt("message"),
+           txt("polymorphism"), txt("encapsulation")],
+          [txt("A concrete instance of a class"),
+           txt("A request made to an object"),
+           txt("Hiding the internal details of a class or object"),
+           txt("Sending the same message to different objects and getting different results"),
+           txt("A specification of an object"),
+           txt("A function that is applied to an object")
+          ])
+    ],
+    [
+        qhead("File Upload"),
+        lin("Submit your homework for the 3n+1 problem as a single .java file"),
+        fup(7, "java"),     
+    ],
+//      match(17, ["class", "object", "method", "message", "polymorphism", "encapsulation"],
+//        ["A concrete instance of a class",
+//         "A request made to an object",
+//         "Hiding the internal details of a class or object",
+//         "Sending the same message to different objects and getting different results",
+//         "A specification of an object"
+//        ])
+    [
+        qhead("Graph Theory"),
+        lin("Find the Shortest Path from vertex 1 to 5.  Leave any cost box blank if the vertex is unreachable."),
+        img("Bellmanford_3.png"),
+        mat(8, 3, 5)
+    ],
+    [
+        qhead("Arithmetic"),
+        txt("What is 2 + 2?"),
+        fil(10)
+    ],
+    [
+        qhead('Dinosaur'),
+        lin("Which one is the dinosaur?"),
+//      mcr(11, [img("cat2.jpg"), img("fish2.png"), img("trex.jpg")])
+    ],
+    [
+        qhead('Dinosaur'),
+        lin("Which one is the dinosaur?"),
+        mcd(12, [img("cat2.jpg"), img("fish2.png"), img("trex.jpg")])
+    ],
+    [
+        qhead('Multimedia'),
+        txt('listen to the following audio file and pick the name of the main character.'),
+        aud("clip1.mp3"),
+        mcd(13, [txt('Yijin'), txt('Asher'), txt('Ying'), txt('Xuefan'), txt('Bob')])
+    ],
+    [
+        qhead('Tacoma Narrows'),
+        txt('Watch the following video, then explain what caused the bridge to fail.'),
+        vid("Tacoma Narrows Bridge Collapse.mp4"),
+        ess(14, 10, 80, 200)
+    ],
+    [
+        qhead('Arithmetic'),
+        txt("What is the square root of 2 (four digits is fine)?"),
+        num(15)
+    ],
+    [
+        qhead("Geography"),
+        lin("Click on Texas"),
+        cli(16,"usmap.gif")
+    ]
     ];
     var q = new Quiz(quizinfo, qlist);   
 }
