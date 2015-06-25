fb5350f283f79a11ca13e92c1e8d56c50b6197ed
shenyubin1990 <shenyubin1990@gmail.com>
2015-06-25 16:53:20 -0400

    MergeSort in Visualcs

---
+++ LiquiZ2/src/org/adastraeducation/visualcs/algorithms/MergeSortApplet.java
@@ -0,0 +1,22 @@
+package org.adastraeducation.visualcs.algorithms;
+
+import org.adastraeducation.visualcs.BasePApplet;
+
+import jogamp.graph.curve.tess.GraphOutline;
+
+@SuppressWarnings("serial")
+public class MergeSortApplet extends BasePApplet {
+	private Array arr = null;
+	public void setup() {
+		super.setup();
+		arr = new Array(null, 10, 0, 99);
+		System.out.println(arr);
+		setDrawer(new PGraphicsGraphDisplayer(this, null, arr));		
+	}
+	public void algorithm() {
+		if (arr == null)
+			return;
+		arr.MergeSort();		
+	}
+}
+
