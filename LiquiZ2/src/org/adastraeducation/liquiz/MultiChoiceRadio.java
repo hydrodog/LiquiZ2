f13e740c036b0e60017b19dd75d05dda09bfe0a7
Jack Tan <nhtzq@users.noreply.github.com>
2015-07-21 15:53:44 -0400

    nothing important.

--- LiquiZ2/src/org/adastraeducation/liquiz/MultiChoiceRadio.java
+++ LiquiZ2/src/org/adastraeducation/liquiz/MultiChoiceRadio.java
@@ -8,13 +8,15 @@ public class MultiChoiceRadio extends MultiChoiceDropdown {
 	public MultiChoiceRadio() {
 	}
 
-	public MultiChoiceRadio(int id, int points, int level, ArrayList<Answer> answers) {
+	public MultiChoiceRadio(int id, int points, int level,
+			ArrayList<Answer> answers) {
 		super(id, points, level, answers);
 	}
+
 	public MultiChoiceRadio(int points, int level, ArrayList<Answer> answers) {
-		super (points, level, answers);
+		super(points, level, answers);
 	}
-	
+
 	public MultiChoiceRadio(int points, int level, String stdChoiceName) {
 		super(points, level, stdChoiceName);
 	}
@@ -22,18 +24,18 @@ public class MultiChoiceRadio extends MultiChoiceDropdown {
 	public MultiChoiceRadio(int id, int points, int level, String stdChoiceName) {
 		super(id, points, level, stdChoiceName);
 	}
-	
+
 	public MultiChoiceRadio(int id, int points, int level) {
 		super(id, points, level);
 	}
-	
+
 	public void writeHTML(DisplayContext dc) {
 		if (dc.isDisplayResponses()) {
 			String[] answer = {};
 			if (dc.getStudentResponses().getLatestResponse(getId()) != null) {
 				answer = dc.getStudentResponses().getLatestResponse(getId());
 			}
-			
+
 			dc.append("<div class='radio'>\n");
 			for (Answer ans : getAns()) {
 				dc.append("<input type='radio' disabled");
@@ -48,11 +50,11 @@ public class MultiChoiceRadio extends MultiChoiceDropdown {
 				dc.append("<br>\n");
 			}
 			dc.append("</div>\n");
-			
+
 			// TODO How to format these responses better?
-			if(answer.length > 0) {
+			if (answer.length > 0) {
 				Response res = getResponseFor(answer[0]);
-				if (res != null) { 
+				if (res != null) {
 					if (Score.correctQues(getId(), answer) == getPoints()) {
 						dc.append("<span class='response correct'>");
 					} else {
@@ -62,7 +64,7 @@ public class MultiChoiceRadio extends MultiChoiceDropdown {
 					dc.append("</span>");
 				}
 			}
-			
+
 			boolean hasAnswer = false;
 			for (Answer ans : getAns()) {
 				if (ans.getCorrect()) {
@@ -70,30 +72,40 @@ public class MultiChoiceRadio extends MultiChoiceDropdown {
 					break;
 				}
 			}
-			if(dc.isDisplayAnswers() && hasAnswer) { //TODO think of a sleeker way to represent this
+			if (dc.isDisplayAnswers() && hasAnswer) { // TODO think of a sleeker
+														// way to represent this
 				dc.append("<span class='answersHead'>Correct answer(s): </span>");
 				for (Answer ans : getAns()) {
-					if(ans.getCorrect()) {
+					if (ans.getCorrect()) {
 						dc.append("<br>");
 						dc.append(ans.getName());
 					}
 				}
-			} 
+			}
 		} else {
 			dc.append("<div class='radio'>\n");
 			for (Answer ans : this.getAns()) {
-				dc.append("<input type='radio' name='" + getId() + "' value='" + ans.getName() + "'> ");
+				dc.append("<input type='radio' name='" + getId() + "' value='"
+						+ ans.getName() + "'> ");
 				ans.writeHTML(dc);
 				dc.append(" <br>\n ");
 			}
-			
+
 			dc.append("</div>\n");
 		}
 	}
 
 	public void writeJS(DisplayContext dc) {
-		writeJS("mcRadio", dc);
+		// q.appendChild(page.mcRadioImg(11, ["cat2.jpg", "fish2.png",
+		// "trex.jpg"]));
+		// if(ans instanceof )
+		dc.append("q.appendChild(page.mcRadioImg(").
+		// if(ans == txt)
+		// / dc.append("q.appendChild(page.mcRadio(").
+				append(getId()).append(", ");
+		// writeJS("mcRadio", dc);
 		Util.writeAnsListAsJS(getAns(), dc);
+		dc.append("));");
 	}
-	
+
 }
