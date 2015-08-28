b659d3c7a37cd8f4f75929c9534867803a914fac
Asher Davidson <usherdavidson@gmail.com>
2015-08-28 12:07:45 -0400

    Fixed bug in Quiz.js where we didn't record data correctly and didn't load saves.

--- LiquiZ2/WebContent/assets/js/Quiz.js
+++ LiquiZ2/WebContent/assets/js/Quiz.js
@@ -15,11 +15,14 @@ function Quiz(payload) {
 
     this.body = document.getElementById("container");
     this.body.className = "quiz";
+}
 
-    this.answers = Array(this.questions.length);
+Quiz.prototype.initAnswers = function() {
+    var a = Array(this.questions.length);
     for (var i = 0; i < this.questions.length; i++) {
-        this.answers[i] = Array(this.questions[i].content.length);
+        a[i] = Array(this.questions[i].content.length);
     }
+    return a;
 }
 
 // TODO(asher): This should be passed in through QuizDemo_ajax
@@ -55,6 +58,7 @@ Quiz.prototype.processParams = function() {
 
 Quiz.prototype.exec = function(refresh) {
     this.processParams();
+    this.answers = this.initAnswers();
     if (refresh || this.view !== url.view) {
         clearPage();
         this.header();
@@ -422,7 +426,9 @@ Quiz.prototype.addInputHandlers = function(input, i, j) {
     var question = this.question_id,
         subQuestion = this.subQuestion_id;
 
-    q = this.answers[question][subQuestion] || Array(0);
+    this.answers = this.answers||this.initAnswers();
+    q = (typeof this.answers[question][subQuestion] !== "undefined") ?
+            this.answers[question][subQuestion] : Array(0);
 
     while (q.length <= i) {
         q.push(Array(0));
@@ -430,9 +436,9 @@ Quiz.prototype.addInputHandlers = function(input, i, j) {
 
     while (q[i].length <= j) {
         q[i].push({
-            v: [],
-            s: [],
-            e: [],
+            v: [], // value
+            s: [], // startTime
+            e: [], // endTime
         });
     }
 
@@ -441,7 +447,7 @@ Quiz.prototype.addInputHandlers = function(input, i, j) {
 
             q = this.answers[question][subQuestion];
 
-            q[i][j].v[q[i][j].v.length-1] = e.target.v;
+            q[i][j].v[q[i][j].v.length-1] = e.target.value;
         }).bind(this);
 
     input.onfocus = (function(e) {
@@ -449,7 +455,7 @@ Quiz.prototype.addInputHandlers = function(input, i, j) {
 
             q = this.answers[question][subQuestion];
 
-            q[i][j].v.push(e.target.v);
+            q[i][j].v.push(e.target.value);
             q[i][j].s.push(new Date().getTime());
         }).bind(this);
 
