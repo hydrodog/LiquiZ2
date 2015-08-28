bbe45ae2810e0708c5a1189933e00c786272614c
Dov Kruger <Dov.Kruger@gmail.com>
2015-08-28 11:51:07 -0400

    pulled out server suffix so server can be any language, not just .jsp

--- LiquiZ2/src/org/adastraeducation/liquiz/servlet/GradeQuiz.java
+++ LiquiZ2/src/org/adastraeducation/liquiz/servlet/GradeQuiz.java
@@ -4,6 +4,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import com.google.gson.*;
+
 public class GradeQuiz extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
 		try {
@@ -11,6 +13,8 @@ public class GradeQuiz extends HttpServlet {
 			BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
 			while ((line = br.readLine()) != null)
 				System.out.println(line);
+			Gson gson = new Gson();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
