98d1197fbff8669df0da57dd200b5d8387af8fa9
stephenjoro <zythlander@host-155-246-209-157.dhcp.stevens-tech.edu>
2015-10-01 14:15:23 -0400

    Added variable name changing.

--- LiquiZ2/WebContent/assets/js/VarWriter.js
+++ LiquiZ2/WebContent/assets/js/VarWriter.js
@@ -26,13 +26,44 @@ function VarWriter(innerHTML, className, id, width, height){
 	div.style.fontFamily = "monospace";
 	this.div = div;
 	div.addEventListener("keydown",(this.keyDown).bind(this));
-	div.valueOf = this.valueOf;
+	div.addEventListener("keydown",(this.keyUp).bind(this));
+	div.valueOf = (this.valueOf).bind(this);
 	this.index = 0;
+	this.tags = [];
 	return div;
 }
+GLOBAL_TAGS = {};
+
+VarWriter.prototype.checkTag = function(tagname){
+	
+};
+
+VarWriter.prototype.tagToHTML = function(tagname){
+	console.log("get: "+tagname);
+	return "cat";
+};
+
+VarWriter.prototype.convertToHTML = function(div, tagname){
+	var tags = [];//div.getElementsByClassName("writervariable");
+	for(var i = 0; i < tags.length; i++){
+		console.log("i["+i+"]: "+tags[i]);
+		//console.log(this.tagToHTML(tags[i].textContent));
+		tags[i].innerHtml = this.tagToHTML(tags[i].textContent);
+		console.log(tags[i].innerHtml);
+		tags[i].setAttribute("class"," ");
+	}
+};
+
 VarWriter.prototype.appendVar = function(match){
-	var tag = Util.span("$"+match.replace("$","")+"$","writervariable");
+	var tag = Util.span(undefined,"writervariable");
+	tag.appendChild(document.createTextNode("$"));
+	var innerSpan = Util.span(match.replace("$",""));
+	innerSpan.contentEditable = true;
+	tag.appendChild(innerSpan);
+	tag.appendChild(document.createTextNode("$"));
+	
 	tag.contentEditable = false;
+	this.tags.push(tag);
 	return tag;
 };
 VarWriter.prototype.tagMatch = function(node,index){
@@ -54,7 +85,29 @@ VarWriter.prototype.tagMatch = function(node,index){
 	return false;
 };
 VarWriter.prototype.valueOf = function(){
-	return this.innerHTML;
+	var nDiv = Util.div();
+	nDiv.innerHTML = this.div.innerHTML;
+	this.convertToHTML(nDiv);
+	return nDiv;
+};
+VarWriter.prototype.keyUp = function(e){
+	var key = e.which;
+	var shift = e.shiftKey;
+	if(window.getSelection){
+		var sel = window.getSelection();
+		var node = sel.anchorNode;
+		var writer = node.parentElement.parentElement;
+		var range = sel.getRangeAt(0);
+		var startIndex = range.startOffset;
+		if(writer.className == "writervariable"){
+			if(startIndex == 1 && key == 8){
+				writer.parentElement.removeChild(writer);
+				e.preventDefault();
+				e.stopPropagation();
+			}
+		}
+	}
+
 };
 VarWriter.prototype.keyDown = function(e){
 	var key = e.which;
@@ -63,39 +116,44 @@ VarWriter.prototype.keyDown = function(e){
 		if(window.getSelection){
 			var sel = window.getSelection();
 			var node = sel.anchorNode;
-			var range = sel.getRangeAt(0);
-			var startIndex = range.startOffset;
-			if(!range.collapsed){
-				var match = range.cloneContents().textContent;
-				match = match.replace("$","");
-				if(match.length > 0){
-					range.deleteContents();
-					var tag = this.appendVar(match);
-					range.insertNode(tag);
-					range.selectNode(tag);
-					sel.removeAllRanges();
-					sel.addRange(range);
-					sel.collapseToEnd();
-					e.preventDefault();
-					e.stopPropagation();
-					if(this.div.oninput)
-						this.div.oninput();
-				}
+			var writer = node.parentElement.parentElement;
+			if(writer.className == "writervariable"){
+				//if needed
 			}else{
-				var match = this.tagMatch(node,startIndex);
-				if(match){
-					var tag = this.appendVar(match);
-					range.setStart(node,this.index);
-					range.setEnd(node,this.index);
-					range.insertNode(tag);
-					range.selectNode(tag);
-					sel.removeAllRanges();
-					sel.addRange(range);
-					sel.collapseToEnd();
-					e.preventDefault();
-					e.stopPropagation();
-					if(this.div.oninput)
-						this.div.oninput();
+				var range = sel.getRangeAt(0);
+				var startIndex = range.startOffset;
+				if(!range.collapsed){
+					var match = range.cloneContents().textContent;
+					match = match.replace("$","");
+					if(match.length > 0){
+						range.deleteContents();
+						var tag = this.appendVar(match);
+						range.insertNode(tag);
+						range.selectNode(tag);
+						sel.removeAllRanges();
+						sel.addRange(range);
+						sel.collapseToEnd();
+						e.preventDefault();
+						e.stopPropagation();
+						if(this.div.oninput)
+							this.div.oninput();
+					}
+				}else{
+					var match = this.tagMatch(node,startIndex);
+					if(match){
+						var tag = this.appendVar(match);
+						range.setStart(node,this.index);
+						range.setEnd(node,this.index);
+						range.insertNode(tag);
+						range.selectNode(tag);
+						sel.removeAllRanges();
+						sel.addRange(range);
+						sel.collapseToEnd();
+						e.preventDefault();
+						e.stopPropagation();
+						if(this.div.oninput)
+							this.div.oninput();
+					}
 				}
 			}
 		}
